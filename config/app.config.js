a150b998dc808ba247e4060013583afc750208ad
Denzel Suhbisi <suhdenzo04@gmail.com>
2023-06-07 15:48:17 +0100

    Added data to the files and fixed server issues

--- config/app.config.js
+++ config/app.config.js
@@ -4,6 +4,6 @@ const MONGO_DB_CONFIG = {
     DB: 'mongodb+srv://suhdenzel:ftAD!.xsM_VB2BA@cluster0.tgr0x2n.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
 }
 
-module.exports = (
+module.exports = {
     MONGO_DB_CONFIG
-)
+}
