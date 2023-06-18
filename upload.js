0d236b91f8c1f3da04767c983cb35c63e6931096
Denzel Suhbisi <suhdenzo04@gmail.com>
2023-06-18 17:57:23 +0100

    fixd images

--- upload.js
+++ upload.js
@@ -2,78 +2,99 @@
 use('ReactBackendProject');
 
 // Insert a few documents into the PortfolioData collection.
-// db.getCollection('projects').insertMany(
-//     [
-//         {
+db.getCollection('projects').insertMany(
+    [
+        {
           
-//             image: 'https://cdn.dribbble.com/userupload/7518943/file/original-b39a16475b25ac10559e04034bd8b9b7.png?compress=1&resize=1504x1128',
-//             title: 'Online Crypto Casino',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/21632973-Online-Crypto-Casino-Mobile-Designs',
-//         },
-//         {
+            image: 'https://cdn.dribbble.com/userupload/7898778/file/original-944f5335f22913ed9caee242690d169d.jpg?compress=1',
+            title: 'Cube Aesthetics',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
        
-//             image: 'https://cdn.dribbble.com/userupload/7531024/file/original-28960363b0b1e5a03c2f6f1f077c298e.jpg?compress=1&resize=1504x1128',
-//             title: 'Education App UIX',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/21637001-Education-App-UIX',
-//         },
-//         {
-//             image: 'https://cdn.dribbble.com/userupload/7474794/file/original-ec5e3ff0615f4ec018a8662d76206bde.jpg?compress=1&resize=1504x1126',
-//             title: 'Recipe app design',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/21617723-Recipe-app-design',
-//         },
-//         {
+            image: 'https://cdn.dribbble.com/userupload/7898779/file/original-3c59f77d90626a5076d657ed34a4fa65.png?compress=1',
+            title: 'Education App UIX',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
+            image: 'https://cdn.dribbble.com/userupload/7898780/file/original-47f1fcc8a1b79a045b644aa9a37ca8ad.jpg?compress=1',
+            title: 'Recipe app design',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
         
-//             image: 'https://cdn.dribbble.com/userupload/7527073/file/original-a56488a4a04a23bfbeb97cafc030dde5.png?compress=1&resize=1504x1128',
-//             title: 'OnStep - Shoes Store',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/21635811-OnStep-Shoes-Store',
-//         },
-//         {
+            image: 'https://cdn.dribbble.com/userupload/7898785/file/original-4578e71bf0af8ba17a1e1144e7d7b494.png?compress=1',
+            title: 'OnStep - Shoes Store',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
      
-//             image: 'https://cdn.dribbble.com/users/5147614/screenshots/17480773/media/77bbee6dafd811f65d14bd38f4c426ab.png?compress=1&resize=1200x900&vertical=top',
-//             title: 'Mobile App Exploration',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/17480773-NWS-Mobile-App-Exploration',
-//         },
-//         {
+            image: 'https://cdn.dribbble.com/userupload/7898782/file/original-1870d052ffe1a00e44c550be54787566.jpg?compress=1',
+            title: 'Mobile App Exploration',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
        
-//             image: 'https://cdn.dribbble.com/userupload/7528445/file/original-4b13d4c68619e9e932565427083bb19d.jpg?compress=1&resize=1504x1126',
-//             title: 'Recipe app',
-//             github: 'https://github.com',
-//             demo: 'https://dribbble.com/shots/21636337-Recipe-app',
-//         }
+            image: 'https://cdn.dribbble.com/userupload/7898781/file/original-2e35d60dc93ee1d55650a9166941addc.jpg?compress=1',
+            title: 'Recipe app',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
+       
+            image: 'https://cdn.dribbble.com/userupload/7898786/file/original-1008ed15dba30d220a2b8a553460f4e0.jpg?compress=1',
+            title: 'Recipe app',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
+       
+            image: 'https://cdn.dribbble.com/userupload/7898784/file/original-678fef2288fa8b45989d0cf95d9b755d.jpg?compress=1',
+            title: 'Recipe app',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        },
+        {
+       
+            image: 'https://cdn.dribbble.com/userupload/7898783/file/original-6968dfad0b83f9aab4547b09c427f97c.jpg?compress=1',
+            title: 'Recipe app',
+            github: 'https://github.com',
+            demo: 'https://dribbble.com/shots/21771767-Backend-Photos?added_first_shot=true',
+        }
 
-//     ]
-// );
+    ]
+);
 
 // Insert a few documents into the PortfolioData collection.
-db.getCollection('testimonials').insertMany(
- [
-        {
-          avatar: 'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2019/01/tips-for-professional-portraits.jpg',
-          name: 'James Brown',
-          review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-        },
+// db.getCollection('testimonials').insertMany(
+//  [
+//         {
+//           avatar: 'https://cdn.fstoppers.com/styles/large-16-9/s3/lead/2019/01/tips-for-professional-portraits.jpg',
+//           name: 'James Brown',
+//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+//         },
         
-        {
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3rvvFzP2v-40awpf_uw6dq1UVh8R3jkGM2rrrqIbd6Ih3_d_fCEW8Fn0sl5OsJ1qaLuo&usqp=CAU',
-          name: 'Tina Snow ',
-          review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-        },
-        {
-          avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJpfFkpLYgdR4R0-ZomZ0ixnZVgLQUOnZnWQ&usqp=CAU',
-          name: 'Kevin Roussel',
-          review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-        },
-        {
-          avatar: 'https://images.squarespace-cdn.com/content/v1/530ce8d1e4b067ea68a9f821/1612484390216-5NVBC0NJJTFP1OPNRU6F/corporate%2Bbusiness%2Bheadshots%2Blos%2Bangeles_Danielle%2BSpires.jpg',
-          name: 'Louisa McBrown',
-          review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
-        },
+//         {
+//           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3rvvFzP2v-40awpf_uw6dq1UVh8R3jkGM2rrrqIbd6Ih3_d_fCEW8Fn0sl5OsJ1qaLuo&usqp=CAU',
+//           name: 'Tina Snow ',
+//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+//         },
+//         {
+//           avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJpfFkpLYgdR4R0-ZomZ0ixnZVgLQUOnZnWQ&usqp=CAU',
+//           name: 'Kevin Roussel',
+//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+//         },
+//         {
+//           avatar: 'https://images.squarespace-cdn.com/content/v1/530ce8d1e4b067ea68a9f821/1612484390216-5NVBC0NJJTFP1OPNRU6F/corporate%2Bbusiness%2Bheadshots%2Blos%2Bangeles_Danielle%2BSpires.jpg',
+//           name: 'Louisa McBrown',
+//           review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus velit omnis deserunt corrupti perferendis quisquam officia cupiditate ut impedit magnam iusto numquam odio eos, itaque, explicabo ipsum esse dolorem unde!"
+//         },
       
       
-      ]
-);
+//       ]
+// );
